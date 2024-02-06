838ef5e16e33a807708ce6c20d10a40418cb39f4
leandrovitor <leandrovitorlelis@gmail.com>
2024-02-06 19:53:48 -0300

    Add files via upload

---
+++ Malhando1/Malhando/Malhando.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "ginastica.h"
+using namespace std;
+
+int main()
+{
+	cout << "Exercicios "
+		<< "de hoje:"
+		<< endl;
+
+    flexao(10);
+	abdominal(20);
+
+	system("pause");
+}
